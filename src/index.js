e89253d5006d27ce7323e55ef21ababb357703f7
Sanchit Kumar Mahto <58548392+sanchy2001@users.noreply.github.com>
2024-08-27 13:23:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-// Get the root element from the DOM
 const container = document.getElementById('root');
 
-// Create a root using React 18's createRoot API
 const root = createRoot(container);
 
-// Render the application
 root.render(
   <Provider store={store}>
     <App />
